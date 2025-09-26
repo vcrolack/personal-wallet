2e87ec12497970701fb6dda334d07c62b17525f7
Victor Rolack <victorolack@gmail.com>
2025-09-26 18:21:31 -0300

    refactor: supabase config

--- src/app/core/integrations/supabase.client.ts
+++ src/app/core/integrations/supabase.client.ts
@@ -1,6 +1,12 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../../environments/environment';
+import { InjectionToken, Provider } from '@angular/core';
+
+export const SUPABASE = new InjectionToken<SupabaseClient>('SUPABASE');
 
 const { url, anonKey } = environment.supabaseConfig;
 
-export const supabase = createClient(url, anonKey);
+export const provideSupabase = (): Provider => ({
+  provide: SUPABASE,
+  useFactory: () => createClient(url, anonKey),
+});
