5ca2e256bac971d3797a3ba0d1387dbb12952213
Victor Rolack <victorolack@gmail.com>
2025-12-25 15:35:37 -0300

    feat(login): sign in implemented

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
 
+  public isLoading = signal(false);
+  public errorMessage = signal<string | null>(null);
+
+  public loginForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true,
+    }),
+    password: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(25),
+      ],
+      nonNullable: true,
+    }),
+  });
+
+  public onSubmit(): void {
+    if (this.loginForm.invalid) return;
+    console.log('lolazo');
+
+    const { email, password } = this.loginForm.getRawValue();
+
+    this.isLoading.set(true);
+
+    this.authService.login(email, password).subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        this.isLoading.set(false);
+        this.errorMessage.set(err.message);
+      },
+    });
+  }
 }
