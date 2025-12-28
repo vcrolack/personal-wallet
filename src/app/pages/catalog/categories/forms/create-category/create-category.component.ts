6ed1de13ef1d428e1b513b89a1fb0c93f81b34d6
Victor Rolack <victorolack@gmail.com>
2025-12-28 19:51:42 -0300

    feat: create category works

--- src/app/pages/catalog/categories/forms/create-category/create-category.component.ts
+++ src/app/pages/catalog/categories/forms/create-category/create-category.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { InputComponent } from '../../../../../common/components/input/input.component';
 import { ButtonComponent } from '../../../../../common/components/button/button.component';
+import { CategoryService } from '../../../../../core/services/category.service';
 
 @Component({
   selector: 'app-create-category',
-  imports: [InputComponent, ButtonComponent],
+  imports: [ReactiveFormsModule, InputComponent, ButtonComponent],
   templateUrl: './create-category.component.html',
   styleUrl: './create-category.component.css',
 })
-export class CreateCategoryComponent {}
+export class CreateCategoryComponent {
+  private fb = inject(FormBuilder);
+  private categoryService = inject(CategoryService);
+  public refreshCategories = output<void>();
+  public closeModal = output<void>();
+
+  public form: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+  });
+
+  public createCategory() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.categoryService.create(this.form.value).subscribe({
+      next: (response) => {
+        console.log('Categoría creada:', response);
+        this.form.reset();
+        this.refreshCategories.emit();
+        this.closeModal.emit();
+      },
+      error: (error) => {
+        console.error('Error al crear categoría:', error);
+      },
+    });
+  }
+}
