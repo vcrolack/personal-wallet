8bf6f91e897c7b5c023cb0ad467c6f9235f7a5d8
Victor Rolack <victorolack@gmail.com>
2025-12-30 17:26:58 -0300

    feat: add wrapper component

---
+++ src/app/common/components/wrapper/wrapper.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wrapper',
+  imports: [],
+  templateUrl: './wrapper.component.html',
+  styleUrl: './wrapper.component.css',
+})
+export class WrapperComponent {}
