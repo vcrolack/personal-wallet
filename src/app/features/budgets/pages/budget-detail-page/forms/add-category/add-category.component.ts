46a00656ae3e016a19a5ea944d0cdb8c4000a7cc
Victor Rolack <victorolack@gmail.com>
2026-01-09 12:11:51 -0300

    feat: create add category form on budget page

--- src/app/features/budgets/pages/budget-detail-page/forms/add-category/add-category.component.ts
+++ src/app/features/budgets/pages/budget-detail-page/forms/add-category/add-category.component.ts
@@ -1,9 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, effect } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CategoryService } from '../../../../../../core/services/category.service';
+import { BudgetCategoryValuesService } from '../../../../../../core/services/budget-category-values.service';
+import {
+  SelectOption,
+  SelectComponent,
+} from '../../../../../../common/components/form/select/select.component';
+import { InputComponent } from '../../../../../../common/components/form/input/input.component';
+import { ButtonComponent } from '../../../../../../common/components/form/button/button.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-category',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    SelectComponent,
+    InputComponent,
+    ButtonComponent,
+  ],
   templateUrl: './add-category.component.html',
   styleUrl: './add-category.component.css',
 })
-export class AddCategoryComponent {}
+export class AddCategoryComponent {
+  private categoryService = inject(CategoryService);
+  private categoryValueService = inject(BudgetCategoryValuesService);
+  private fb = inject(FormBuilder);
+
+  public categoryResource = this.categoryService.categoryResource;
+  public categoryValuesResource =
+    this.categoryValueService.categoryValuesResource;
+
+  public categoriesForSelect = computed(
+    (): SelectOption[] =>
+      this.categoryResource.value()?.map((category) => ({
+        label: category.name,
+        value: category.id,
+      })) ?? []
+  );
+
+  public form: FormGroup = this.fb.group({
+    category: [null, Validators.required],
+    categoryValue: [null, Validators.required],
+    amount: [null, Validators.required],
+  });
+
+  public categoryId = toSignal(this.form.get('category')!.valueChanges, {
+    initialValue: null,
+  });
+
+  public categoryValueId = toSignal(
+    this.form.get('categoryValue')!.valueChanges,
+    {
+      initialValue: null,
+    }
+  );
+
+  public categoryValuesForSelect = computed(
+    (): SelectOption[] =>
+      this.categoryValuesResource.value()?.map((categoryValue) => ({
+        label: categoryValue.name,
+        value: categoryValue.id,
+      })) ?? []
+  );
+
+  constructor() {
+    effect(() => {
+      const categoryId = this.categoryId();
+      if (categoryId) {
+        this.categoryValueService.selectCategory(categoryId);
+      }
+    });
+  }
+}
